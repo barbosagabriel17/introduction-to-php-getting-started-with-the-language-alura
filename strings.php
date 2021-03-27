9ac9031b0195ec5a866581dcf06d88af746a1242
barbosagabriel17 <barbosagabriel@outlook.com>
2021-03-27 08:28:48 -0300

    Aula 3

---
+++ strings.php
@@ -0,0 +1,10 @@
+<?php
+
+$idade = 25;
+echo 'Olá mundo! Minha idade é ' . $idade . 'anos'; //Em PHP o ponto faz a concatenação
+
+echo "\n"; // /n só funciona com aspas duplas
+
+echo "\tOlá mundo! Minha idade é $idade anos\n"; //Se utilizar aspas duplas não preciso utilizar o símbolo ponto para concatenar
+                                                // /t é um tab    
+echo "Testando uma funcionalidade do PHP" .PHP_EOL;           
